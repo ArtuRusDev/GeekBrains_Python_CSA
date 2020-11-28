ff660a31768f2db11033bc80e0c02741bcdc59cd
Artur Ilin <ilinartur2@gmail.com>
2020-11-29 02:14:43 +0800

    Add les_2 files

---
+++ Lesson_2_Artur_Ilin/task_2/main.py
@@ -0,0 +1,63 @@
+"""
+2. Задание на закрепление знаний по модулю json. Есть файл orders
+в формате JSON с информацией о заказах. Написать скрипт, автоматизирующий
+его заполнение данными.
+
+Для этого:
+Создать функцию write_order_to_json(), в которую передается
+5 параметров — товар (item), количество (quantity), цена (price),
+покупатель (buyer), дата (date). Функция должна предусматривать запись
+данных в виде словаря в файл orders.json. При записи данных указать
+величину отступа в 4 пробельных символа;
+Проверить работу программы через вызов функции write_order_to_json()
+с передачей в нее значений каждого параметра.
+
+ПРОШУ ВАС НЕ УДАЛЯТЬ ИСХОДНЫЙ JSON-ФАЙЛ
+ПРИМЕР ТОГО, ЧТО ДОЛЖНО ПОЛУЧИТЬСЯ
+
+{
+    "orders": [
+        {
+            "item": "printer",
+            "quantity": "10",
+            "price": "6700",
+            "buyer": "Ivanov I.I.",
+            "date": "24.09.2017"
+        },
+        {
+            "item": "scaner",
+            "quantity": "20",
+            "price": "10000",
+            "buyer": "Petrov P.P.",
+            "date": "11.01.2018"
+        }
+    ]
+}
+
+вам нужно подгрузить JSON-объект
+и достучаться до списка, который и нужно пополнять
+а потом сохранять все в файл
+"""
+
+import json
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    with open('orders.json', 'r') as f_n:
+        json_content = json.load(f_n)
+
+    add_dict = {
+        'item': item,
+        'quantity': quantity,
+        'price': price,
+        'buyer': buyer,
+        'date': date
+    }
+
+    json_content['orders'].append(add_dict)
+
+    with open('orders.json', 'w') as f_n:
+        json.dump(json_content, f_n, indent=4)
+
+
+write_order_to_json("laptop", "8", "12700", "Andreed I.A.", "15.08.2002")
